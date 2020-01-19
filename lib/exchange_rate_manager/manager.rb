50f1697166a80f04dcfb4bac3e417decf13b6d56
Paul Hernández <mpaul.hernandez@gmail.com>
2020-01-19 01:22:46 -0600

    Implement changes in ExchangeRateManager::Manager to pass tests

--- lib/exchange_rate_manager/manager.rb
+++ lib/exchange_rate_manager/manager.rb
@@ -1,15 +1,29 @@
 # Provides an interface for working with exchange rates
 module ExchangeRateManager
   class Manager
-    def new
+    def initialize
     end
 
-    def fetch_exchange_rate(fetcher: nil)
-      return true
+    def fetch_exchange_rate(day:, fetcher: nil)
+      value = fetcher.fetch_exchange_rate(day: day)
+
+      return {
+        "requested_day" => day,
+        "exchange_rate_found" => (true.nil? ? false : true),
+        "exchange_rate_value" => (value.nil? ? nil : value.to_f),
+        "errors" => fetcher.errors
+      }
     end
 
-    def insert_exchange_rate(inserter: nil)
-      return true
+    def insert_exchange_rate(day:, value:, inserter: nil)
+      inserted = inserter.insert_exchange_rate(day: day, value: value)
+
+      return {
+        "requested_day" => day,
+        "exchange_rate_value" => value.to_f,
+        "exchange_rate_inserted" => inserted,
+        "errors" => inserter.errors
+      }
     end
   end
 end
