765958f623b8f6142a9e6c7bafb058292c0d0819
Paul Hernández <mpaul.hernandez@gmail.com>
2020-01-20 23:02:18 -0600

    Define ExchangeRateManager::DBInserter class to pass tests

---
+++ lib/exchange_rate_manager/db_inserter.rb
@@ -0,0 +1,14 @@
+module ExchangeRateManager
+  class DBInserter
+    def initialize
+    end
+
+    def test_connection
+      return true
+    end
+
+    def insert_exchange_rate(day:, value:)
+      return true
+    end
+  end
+end
