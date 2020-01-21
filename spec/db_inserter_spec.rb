9ccd0024449a8fc583971ebf6dbb8e2ee3b37ef9
Paul Hernández <mpaul.hernandez@gmail.com>
2020-01-21 17:37:19 -0600

    Update definition of ExchangeRateManager::DBInserter's interface in its spec

--- spec/db_inserter_spec.rb
+++ spec/db_inserter_spec.rb
@@ -1,6 +1,21 @@
 require "./env_variables"
 
 RSpec.describe ExchangeRateManager::DBInserter do
+  # No need to test the actual connection to the database
+  class FakeDBConnection
+    def initialize(expected_result = nil)
+      @expected_result = expected_result
+    end
+
+    def exec(query_string = "")
+      @expected_result
+    end
+
+    def close
+      return nil
+    end
+  end
+
   def new_inserter
     ExchangeRateManager::DBInserter.new
   end
@@ -9,11 +24,36 @@ RSpec.describe ExchangeRateManager::DBInserter do
     before(:all) do
       @inserter = new_inserter
     end
-    it "defines #test_connection" do
-      expect(@inserter).to respond_to(:test_connection)
-    end
     it "defines #insert_exchange_rate" do
-      expect(@inserter).to respond_to(:test_connection)
+      expect(@inserter).to respond_to(:insert_exchange_rate)
+    end
+    it "defines #connection_works?" do
+      expect(@inserter).to respond_to(:connection_works?)
+    end
+    it "defines #errors" do
+      expect(@inserter).to respond_to(:errors)
+    end
+    context "when a working connection to the config. DB is present" do
+      describe "#insert_exchange_rate" do
+        # todo
+      end
+      describe "#connection_works?" do
+        it "returns true"
+      end
+      describe "#errors" do
+        it "returns an empty array"
+      end
+    end
+    context "when a working connection to the config. DB is not present" do
+      describe "#insert_exchange_rate" do
+        it "returns nil"
+      end
+      describe "#connection_works?" do
+        it "returns false"
+      end
+      describe "#errors" do
+        it "includes a DB connection error message"
+      end
     end
   end
 end
