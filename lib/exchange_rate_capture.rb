b5e291baed99805496b8311a59933c8115720533
Paul Hernández <mpaul.hernandez@gmail.com>
2020-02-04 11:26:49 -0600

    Remove require for pry gem

--- lib/exchange_rate_capture.rb
+++ lib/exchange_rate_capture.rb
@@ -6,7 +6,6 @@ require_relative "exchange_rate_capture/persisted_record_validator"
 require_relative "exchange_rate_capture/api_fetcher"
 require_relative "exchange_rate_capture/db_inserter"
 require_relative "exchange_rate_capture/capturist"
-require "pry"
 
 module ExchangeRateCapture
   class Error < StandardError; end
